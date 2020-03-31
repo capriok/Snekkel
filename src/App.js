00802530eccbe40cff9588decc67c6cdad18f3cc
Kyle Caprio <capriok7@yahoo.com>
2020-03-30 19:21:46 -0700

    tail logic

--- src/App.js
+++ src/App.js
@@ -4,64 +4,94 @@ import './App.scss';
 function App() {
   let rows = 31
   let cols = 31
-  let tickTime = 125
-  let death = false
+  let tickTime = 100
+
+  const [dead, isDead] = useState(false)
+  const getRandomFoodPos = () => {
+    return {
+      row: Math.floor((Math.random() * rows)),
+      col: Math.floor((Math.random() * cols))
+    }
+  }
 
   const [grid, setGrid] = useState([])
   const [ticker, setTick] = useState(0)
   const [currentDirection, setDirection] = useState('up')
-  // const [food, setFood] = useState({
-  //   row: Math.floor((Math.random() * rows)),
-  //   col: Math.floor((Math.random() * cols))
-  // })
   const [food, setFood] = useState({
     row: 15,
-    col: 16
+    col: 15
+
   })
   const [snake, setSnake] = useState({
     head: {
-      row: Math.floor(34),
+      row: Math.floor(30),
       col: Math.floor((cols) / 2)
     },
     tail: []
   })
 
-  // draw grid and initially place food and head
+  // draw grid and place food and head
   useEffect(() => {
+    // console.log(snake.tail);
+
     let population = []
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
         const isFood = (food.row === row && food.col === col)
         const isHead = (snake.head.row === row && snake.head.col === col)
-        population.push({ row, col, isFood, isHead })
+        let isTail = false;
+        snake.tail.forEach(t => {
+          if (t.row === row && t.col === col) {
+            isTail = true;
+          }
+        })
+        population.push({ row, col, isFood, isHead, isTail })
       }
     }
     setGrid(population)
-  }, [food, snake.head, snake.tail])
+  }, [ticker])
 
-  // game ticker
-  const tickGame = () => {
-    setTimeout(() => {
+  // eat food
+  const eatFood = () => {
+    console.log(snake.tail);
+    snake.tail.push({
+      row: snake.head.row,
+      col: snake.head.col
+    })
+    setFood(getRandomFoodPos)
+  }
+
+
+  // game tick
+  useEffect(() => {
+    const gameTick = setTimeout(() => {
       setTick(ticker + tickTime)
 
       const isEating = (snake.head.row === food.row && snake.head.col === food.col)
       isEating && eatFood()
     }, tickTime)
-  }
 
-  const eatFood = () => {
-    let { tail } = snake.tail
-    let newTail = {
-      row: snake.head.row - 1,
-      col: snake.head.col - 1
-    }
-    setSnake({ ...snake, tail: [newTail] })
-
-  }
-
-  // snake direction
-  useEffect(() => {
     let { row, col } = snake.head
+    let tail = snake.tail
+
+
+    snake.tail.forEach((piece) => {
+      if (piece.row === snake.head.row && piece.col === snake.head.col) {
+        isDead(true)
+        console.log(dead);
+        clearTimeout(gameTick)
+      }
+    })
+    // isDead && 
+
+    tail.unshift({
+      row: row,
+      col: col,
+    })
+
+    tail.pop()
+
+
     switch (currentDirection) {
       case 'up':
         if (row === 0) {
@@ -100,35 +130,33 @@ function App() {
   const handleControls = (e) => {
     switch (e.keyCode) {
       case 87:
-        currentDirection !== 'down' && setDirection('up')
+        setDirection(currentDirection => currentDirection !== 'down' ? 'up' : currentDirection)
         break;
       case 68:
-        currentDirection !== 'left' && setDirection('right')
+        setDirection(currentDirection => currentDirection !== 'left' ? 'right' : currentDirection)
         break;
       case 83:
-        currentDirection !== 'up' && setDirection('down')
+        setDirection(currentDirection => currentDirection !== 'up' ? 'down' : currentDirection)
         break;
       case 65:
-        currentDirection !== 'right' && setDirection('left')
+        setDirection(currentDirection => currentDirection !== 'right' ? 'left' : currentDirection)
         break;
       default:
         break;
     }
   }
 
-  tickGame()
-  console.log(snake);
-
   return (
     <div className="App" tabIndex="0"
       onKeyDown={() => document.addEventListener('keydown', handleControls)}
     >
       <div className="grid">
-        {grid.map(({ row, col, isFood, isHead }) => (
+        {grid.map(({ row, col, isFood, isHead, isTail }) => (
           <div key={`${row}-${col}`} className={
             isHead
               ? "grid-item is-head" : isFood
-                ? "grid-item is-food" : "grid-item"
+                ? "grid-item is-food" : isTail
+                  ? "grid-item is-tail" : "grid-item"
           }></div>
         ))}
       </div>
