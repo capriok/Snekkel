c1ebe7ef5826ef147a081f485d1a530acac53548
Kyle Caprio <capriok7@yahoo.com>
2020-04-03 19:05:21 -0700

    food, drawer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import 'react-tippy/dist/tippy.css'
 
 function App() {
   const isMobile = window.innerWidth < 500
-  const [drawer, openDrawer] = useState(false)
+  const [drawer, setDrawer] = useState(false)
 
   let rows = 31
   let cols = 31
@@ -29,12 +29,12 @@ function App() {
   const [tickTime, setTickTime] = useState(150)
   const [ticker, setTick] = useState(0)
   const [grid, setGrid] = useState([])
-  const [snake, setSnake] = useState({ head: { row: Math.floor(32), col: Math.floor((cols) / 2) }, tail: [] })
+  const [snake, setSnake] = useState({ head: { row: Math.floor(32), col: Math.floor((cols) / 2) }, body: [] })
   const [food, setFood] = useState(getRandomFoodPos)
   const [currentDirection, setDirection] = useState('up')
 
 
-  const getDirection = (curr, prev, next) => {
+  const getDirection = (curr, prev) => {
     if (prev) {
       if (curr.col === prev.col && curr.row - 1 === prev.row) {
         return "up";
@@ -58,18 +58,18 @@ function App() {
         for (let col = 0; col < cols; col++) {
           const isFood = (food.row === row && food.col === col)
           const isHead = (snake.head.row === row && snake.head.col === col)
+          let isBody = false;
           let isTail = false;
-          let isLastTail = false;
           let direction = "";
-          snake.tail.forEach((t, index) => {
+          snake.body.forEach((t, index) => {
             if (t.row === row && t.col === col) {
-              isTail = true;
-              direction = getDirection(t, snake.tail[index - 1], snake.tail[index + 1]);
-              isLastTail = snake.tail.length - 1 === index;
+              isBody = true;
+              direction = getDirection(t, snake.body[index - 1]);
+              isTail = snake.body.length - 1 === index;
             }
           })
 
-          population.push({ row, col, isFood, isHead, isTail, isLastTail, direction })
+          population.push({ row, col, isFood, isHead, isBody, isTail, direction })
         }
       }
       setGrid(population)
@@ -81,7 +81,6 @@ function App() {
   useEffect(() => {
     document.addEventListener('keydown', handleControls)
     let { row, col } = snake.head
-    let tail = snake.tail
     //tick the game
     const gameTick = setTimeout(() => {
       !dead && setTick(ticker + tickTime)
@@ -89,10 +88,9 @@ function App() {
 
       if (snake.head.row === food.row && snake.head.col === food.col) {
         setFood(getRandomFoodPos)
-        snake.tail.push({
+        snake.body.push({
           row: snake.head.row,
-          col: snake.head.col,
-          isBody: true
+          col: snake.head.col
         })
 
         // increase snake speed based on difficulty
@@ -112,7 +110,7 @@ function App() {
     }, tickTime)
 
     // head collides with tail then reset game
-    snake.tail.forEach((piece) => {
+    snake.body.forEach((piece) => {
       if (piece.row === snake.head.row && piece.col === snake.head.col) {
         isDead(true)
         clearTimeout(gameTick)
@@ -120,8 +118,8 @@ function App() {
     })
 
     // move tail
-    tail.unshift({ row: row, col: col, })
-    tail.pop()
+    snake.body.unshift({ row: row, col: col, })
+    snake.body.pop()
     if (difficulty === 'easy') {
       switch (currentDirection) {
         case 'up':
@@ -196,15 +194,19 @@ function App() {
   const handleControls = (e) => {
     switch (e.keyCode) {
       case 87:
+      case 38:
         setDirection(currentDirection => currentDirection !== 'down' ? 'up' : currentDirection)
         break;
       case 68:
+      case 39:
         setDirection(currentDirection => currentDirection !== 'left' ? 'right' : currentDirection)
         break;
       case 83:
+      case 40:
         setDirection(currentDirection => currentDirection !== 'up' ? 'down' : currentDirection)
         break;
       case 65:
+      case 37:
         setDirection(currentDirection => currentDirection !== 'right' ? 'left' : currentDirection)
         break;
       default:
@@ -222,61 +224,64 @@ function App() {
     switch (difficulty) {
       case 'easy':
         setTickTime(100)
-        console.log('tickTime', tickTime);
         break;
       case 'hard':
         setTickTime(70)
-        console.log('tickTime', tickTime);
         break;
       case 'insane':
         setTickTime(40)
-        console.log('tickTime', tickTime);
         break;
       default:
-        console.log('difficulty', difficulty);
-        ;
+        break;
     }
     setFood(getRandomFoodPos)
     setDirection('up')
-    setSnake({ head: { row: Math.floor(30), col: Math.floor(15) }, tail: [] })
+    setSnake({ head: { row: Math.floor(30), col: Math.floor(15) }, body: [] })
     isDead(false)
   }
 
-  const paintGrid = (isFood, isHead, isTail, isLastTail, direction) => {
+  const paintGrid = (isFood, isHead, isBody, isTail, direction) => {
 
     let baseGridItem = "grid-item";
 
     if (isHead) { return baseGridItem + " is-head " + currentDirection; }
-    if (isLastTail) { return baseGridItem + " is-last-tail " + direction; }
-    if (isTail) { return baseGridItem + " snake-body " + direction; }
-    if (isFood) { return baseGridItem + " is-food "; }
+    if (isTail) { return baseGridItem + " is-tail " + direction; }
+    if (isBody) { return baseGridItem + " is-body " + direction; }
+    if (isFood) { return baseGridItem + ` is-food ${Math.floor(Math.random(1 * 6))}`; }
     return baseGridItem;
 
   }
 
-
-  // isHead
-  // ? "grid-item is-head " + currentDirection : isFood
-  //   ? "grid-item is-food" : isTail
-  //     ? "grid-item snake-body" : isLastTail
-  //       ? "grid-item isLastTail" : "grid-item"
-
+  const toggleHelp = () => {
+    isHelping(() => isHelping(true))
+    setDrawer(false)
+    setTimeout(() => {
+      isHelping(() => isHelping(false))
+    }, 5000)
+  }
 
   return (
     <>
       {drawer &&
-        <Drawer onClick={() => openDrawer(false)} open={drawer} bg="rgb(17, 17, 17)" color="white" >
-          <h1>Title</h1>
-          <p>This is some placeholder content</p>
+        <Drawer onClick={() => setDrawer(false)} open={drawer} bg="rgb(17, 17, 17)" color="white" padding="0px" >
+          <div className="drawer">
+            <h1>Game</h1>
+            <p onClick={() => {
+              toggleHelp()
+            }}>Objective</p>
+            <h1>Navigation</h1>
+            <p><a href="https://www.kylecaprio.dev">Portfolio</a></p>
+            <p><a href="https://disarray.kylecaprio.dev">Disarray</a></p>
+          </div>
         </Drawer>
       }
       <div className="App">
         <Navbar className="nav" title="Kyle Caprio" shadow>
-          <NavLink onClick={() => openDrawer(!drawer)}><h1>≡</h1></NavLink>
+          <NavLink onClick={() => setDrawer(!drawer)}><h1>≡</h1></NavLink>
         </Navbar>
         {help && <div className="help">
-          <div>WASD to move</div>
-          <div>Only your tail kills you</div>
+          <div>WASD or Arrows to move</div>
+          <div>Only your body kills you</div>
         </div>}
         {isMobile
           ? <div className="unavailable">
@@ -292,8 +297,8 @@ function App() {
                   <button onClick={resetGame}>Start Game</button>
                 </div>
               </div>
-              : !dead && grid.map(({ row, col, isFood, isHead, isTail, isLastTail, direction }) => (
-                <div key={`${row}-${col}`} className={paintGrid(isFood, isHead, isTail, isLastTail, direction)}></div>
+              : !dead && grid.map(({ row, col, isFood, isHead, isBody, isTail, direction }) => (
+                <div key={`${row}-${col}`} className={paintGrid(isFood, isHead, isBody, isTail, direction)}></div>
               ))}
             {(dead && !init) &&
               <div className="modal">
@@ -303,9 +308,8 @@ function App() {
                     <>
                       <h1>Select Difficulty</h1>
                       <div>
-
                         <Tooltip
-                          title="100ms/tick || -2ms/meal, || Tail kills you"
+                          title="100ms/tick || -2ms/meal, || Body kills you"
                           position="bottom"
                           trigger="mouseenter">
                           <button className="easybtn" onClick={() => {
@@ -315,7 +319,7 @@ function App() {
                         </button>
                         </Tooltip>
                         <Tooltip
-                          title="70ms/tick || -3ms/meal, || Tail & Walls kill you"
+                          title="70ms/tick || -3ms/meal, || Body & Walls kill you"
                           position="bottom"
                           trigger="mouseenter">
                           <button className="hardbtn" onClick={() => {
@@ -324,7 +328,7 @@ function App() {
                           }}>Hard</button>
                         </Tooltip>
                         <Tooltip
-                          title="40ms/tick || -4ms/meal, || Tail & Walls kill you"
+                          title="40ms/tick || -4ms/meal, || Body & Walls kill you"
                           position="bottom"
                           trigger="mouseenter">
                           <button className="insanebtn" onClick={() => {
@@ -337,7 +341,7 @@ function App() {
                     </>
                     :
                     <>
-                      <h1>Score: {snake.tail.length} </h1>
+                      <h1>Score: {snake.body.length} </h1>
                       <div>
                         <button className="border" onClick={resetGame}>Restart Game</button>
                         <button className="border" onClick={() => isSelectingDifficulty(true)}>Change Difficulty</button>
